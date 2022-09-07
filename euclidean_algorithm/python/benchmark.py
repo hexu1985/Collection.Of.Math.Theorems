4fd39f68bd40c372d7a85ccbe6d4656c5635a181
hexu <hexu_1985@sina.com>
2022-09-07 17:29:06 +0800

    add python implement

--- euclidean_algorithm/python/benchmark.py
+++ euclidean_algorithm/python/benchmark.py
@@ -3,4 +3,6 @@ import math
 def print_gcd(a, b):
     print("gcd({}, {}) = {}".format(a, b, math.gcd(a, b)))
 
-print_gcd(252, 198)
+if __name__ == "__main__":
+    print_gcd(252, 198)
+    print_gcd(414, 662)
