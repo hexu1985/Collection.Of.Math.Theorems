886cc005b9dd28265d2776849dfa4282bf3accde
hexu <hexu_1985@sina.com>
2022-09-29 18:49:06 +0800

    update cosine_law.py

--- cosine_law/python/cosine_law.py
+++ cosine_law/python/cosine_law.py
@@ -12,5 +12,13 @@ plt.axis([x1,x2,y1,y2])
 plt.axis('on')
 plt.grid(True)
 
+xa = -2; ya = 6;
+xb = 5; yb = 0
+xc = 0; yc = 0;
+
+plt.plot([xa, xb], [ya, yb])
+plt.plot([xa, xc], [ya, yc])
+plt.plot([xb, xc], [yb, yc])
+
 plt.show()
 
