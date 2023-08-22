865b870914bb09aba3d35b924dbd2b4bd83f6a45
hexu <hexu_1985@sina.com>
2023-08-22 18:57:07 +0800

    add  vectorial_angle_cosine.py

---
+++ analytic_geometry/vectorial_angle_cosine/python/vectorial_angle_cosine.py
@@ -0,0 +1,21 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+xmin=-10
+xmax=10
+ymin=-10
+ymax=10
+plt.axis([xmin, xmax, ymin, ymax])
+plt.axis('on')
+plt.axis("equal")
+
+plt.grid(True)
+
+# draw vector a (ax, ay)
+
+# draw vector b (bx, by)
+
+# calculate angle between a and b
+
+plt.show()
+
