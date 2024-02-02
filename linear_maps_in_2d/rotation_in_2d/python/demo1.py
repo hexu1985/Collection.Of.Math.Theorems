03d1fcb74423548cd228bbed5723497e5ead8d85
hexu <hexu_1985@sina.com>
2024-02-02 18:56:53 +0800

    update linear_maps_in_2d/rotation_in_2d

--- linear_maps_in_2d/rotation_in_2d/python/demo1.py
+++ linear_maps_in_2d/rotation_in_2d/python/demo1.py
@@ -4,13 +4,9 @@ from vectors import *
 from vector_drawing import *
 from dino_vectors import dino_vectors
 
-rotation_angle = pi/4
-
-dino_polar = [to_polar(v) for v in dino_vectors]
-dino_rotated_polar = [(l,angle + rotation_angle) for l,angle in dino_polar]
-dino_rotated = [to_cartesian(p) for p in dino_rotated_polar]
-
 draw(
-    Polygon(*dino_vectors, color=gray),
-    Polygon(*dino_rotated, color=red)
+    Points(*dino_vectors),
+    Polygon(*dino_vectors)
 )
+
+
---
+++ linear_maps_in_2d/rotation_in_2d/python/demo2-2.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+from vectors import *
+from vector_drawing import *
+from dino_vectors import dino_vectors
+
+rotation_angle = pi/4
+
+dino_polar = [to_polar(v) for v in dino_vectors]
+dino_rotated_polar = [(l,angle + rotation_angle) for l,angle in dino_polar]
+dino_rotated = [to_cartesian(p) for p in dino_rotated_polar]
+
+arrows = [Segment(tip,tail,color=black) for (tip,tail) in
+         zip(dino_rotated, dino_vectors)]
+draw(
+    Points(*dino_vectors, color=blue),
+    Polygon(*dino_vectors, color=blue),
+    Points(*dino_rotated, color=red),
+    Polygon(*dino_rotated, color=red),
+    *arrows
+)
+
+
