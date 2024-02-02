abf89363f3eee68dd8a2709068ad48a6c9716963
hexu <hexu_1985@sina.com>
2024-02-02 18:35:29 +0800

    update linear_maps_in_2d/translation_in_2d

--- linear_maps_in_2d/translation_in_2d/python/demo2-2.py
+++ linear_maps_in_2d/translation_in_2d/python/demo2-2.py
@@ -4,12 +4,16 @@ from vectors import *
 from vector_drawing import *
 from dino_vectors import dino_vectors
 
-dino_vectors2 = translate((-1.5, -2.5), dino_vectors)
+dino_vectors2 = [add((-1.5,-2.5), v) for v in dino_vectors]
 
+arrows = [Segment(tip,tail,color=black) for (tip,tail) in
+         zip(dino_vectors2, dino_vectors)]
 draw(
     Points(*dino_vectors, color=blue),
     Polygon(*dino_vectors, color=blue),
     Points(*dino_vectors2, color=red),
-    Polygon(*dino_vectors2, color=red)
+    Polygon(*dino_vectors2, color=red),
+    *arrows
 )
+
 
